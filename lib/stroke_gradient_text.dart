d0ba5b61c662c797d74e93aa5719efc0d3cf0a0c
tao123 <1315702128@qq.com>
2025-06-30 21:32:53 +0800

    111

--- lib/stroke_gradient_text.dart
+++ lib/stroke_gradient_text.dart
@@ -1,5 +1,70 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class StrokeAndGradientText extends StatelessWidget {
+  const StrokeAndGradientText({
+    super.key,
+    this.strokeCap = StrokeCap.round,
+    this.strokeJoin = StrokeJoin.round,
+    this.strokeWidth,
+    this.strokeColor,
+    required this.child,
+    this.gradient,
+  });
+
+  final StrokeCap strokeCap;
+  final StrokeJoin strokeJoin;
+  final double? strokeWidth;
+  final Color? strokeColor;
+  final Text child;
+  final Gradient? gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle style;
+    if (child.style != null) {
+      style = child.style!.copyWith(
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeCap = strokeCap
+          ..strokeJoin = strokeJoin
+          ..strokeWidth = strokeWidth ?? 0
+          ..color = strokeColor ?? Colors.transparent,
+        color: null,
+      );
+    } else {
+      style = TextStyle(
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeCap = strokeCap
+          ..strokeJoin = strokeJoin
+          ..strokeWidth = strokeWidth ?? 0
+          ..color = strokeColor ?? Colors.transparent,
+      );
+    }
+    return Stack(
+      alignment: Alignment.center,
+      textDirection: child.textDirection,
+      children: <Widget>[
+        Text(
+          child.data!,
+          style: style,
+          maxLines: child.maxLines,
+          overflow: child.overflow,
+          semanticsLabel: child.semanticsLabel,
+          softWrap: child.softWrap,
+          strutStyle: child.strutStyle,
+          textAlign: child.textAlign,
+          textDirection: child.textDirection,
+          textScaleFactor: child.textScaleFactor,
+        ),
+        gradient != null
+            ? ShaderMask(
+                shaderCallback: (r) => gradient!.createShader(r),
+                blendMode: BlendMode.srcIn,
+                child: child,
+              )
+            : child,
+      ],
+    );
+  }
 }
